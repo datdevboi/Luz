03c05f92a9c20b4093152a643f319918a6299d9a
datdevboi <cami172013@gmail.com>
2018-12-13 19:03:58 -0600

    changed this to null when we call apply on the function

--- src/internals/_curry1.ts
+++ src/internals/_curry1.ts
@@ -5,7 +5,7 @@ function _curry1(fn: any) {
     if (arguments.length === 0 || _isPlaceholder(a)) {
       return f1;
     } else {
-      return fn.apply(this, arguments);
+      return fn.apply(null, arguments);
     }
   };
 }
