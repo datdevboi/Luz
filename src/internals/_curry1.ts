f9e6a1d2eef6c56a15eb9619b6cbbc014ac8423b
datdevboi <cami172013@gmail.com>
2018-12-13 18:41:12 -0600

    created _curry1 that will work on function with the arrity of 1

---
+++ src/internals/_curry1.ts
@@ -0,0 +1,13 @@
+import { _isPlaceholder } from "./_isPlaceholder";
+
+function _curry1(fn: any) {
+  return function f1(a: any) {
+    if (arguments.length === 0 || _isPlaceholder(a)) {
+      return f1;
+    } else {
+      return fn.apply(this, arguments);
+    }
+  };
+}
+
+export { _curry1 };
