a2c04b1c534d5b417fb47b151f3893fa15801009
datdevboi <cami172013@gmail.com>
2018-12-13 18:42:00 -0600

    created _curry3 that will work on function with the arity of 3

---
+++ src/internals/_curry3.ts
@@ -0,0 +1,58 @@
+import { _isPlaceholder } from "./_isPlaceholder";
+import { _curry1 } from "./_curry1";
+import { _curry2 } from "./_curry2";
+
+function _curry3(fn: any) {
+  return function f3(a: any, b: any, c: any) {
+    switch (arguments.length) {
+      case 0:
+        return f3;
+      case 1:
+        return _isPlaceholder(a)
+          ? f3
+          : _curry2((_b: any, _c: any) => fn(a, _b, _c));
+      case 2:
+        return _isPlaceholder(a) && _isPlaceholder(b)
+          ? f3
+          : _isPlaceholder(a)
+          ? _curry2(function(_a: any, _c: any) {
+              return fn(_a, b, _c);
+            })
+          : _isPlaceholder(b)
+          ? _curry2(function(_b: any, _c: any) {
+              return fn(a, _b, _c);
+            })
+          : _curry1(function(_c: any) {
+              return fn(a, b, _c);
+            });
+      default:
+        return _isPlaceholder(a) && _isPlaceholder(b) && _isPlaceholder(c)
+          ? f3
+          : _isPlaceholder(a) && _isPlaceholder(b)
+          ? _curry2(function(_a: any, _b: any) {
+              return fn(_a, _b, c);
+            })
+          : _isPlaceholder(a) && _isPlaceholder(c)
+          ? _curry2(function(_a: any, _c: any) {
+              return fn(_a, b, _c);
+            })
+          : _isPlaceholder(b) && _isPlaceholder(c)
+          ? _curry2(function(_b: any, _c: any) {
+              return fn(a, _b, _c);
+            })
+          : _isPlaceholder(a)
+          ? _curry1(function(_a: any) {
+              return fn(_a, b, c);
+            })
+          : _isPlaceholder(b)
+          ? _curry1(function(_b: any) {
+              return fn(a, _b, c);
+            })
+          : _isPlaceholder(c)
+          ? _curry1(function(_c: any) {
+              return fn(a, b, _c);
+            })
+          : fn(a, b, c);
+    }
+  };
+}
