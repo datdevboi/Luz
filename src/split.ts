4f6b65d35160058a2ff3436ac556cb4a27da1c4c
datdevboi <cami172013@gmail.com>
2018-12-17 17:33:55 -0600

    Removed comments and unnecessary import

--- src/split.ts
+++ src/split.ts
@@ -1,9 +1,4 @@
-import { _curry2 } from "./internals/_curry2";
 import { nativeInvoker } from "./nativeInvoker";
-
-// const split = _curry2(function split(splitby: string | RegExp, a: string) {
-//   return a.split(splitby);
-// });
 
 const split = nativeInvoker(1, "split");
 
