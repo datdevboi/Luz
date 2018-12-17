4874d98288893d183f4fa4fa47a3644ce8fb908b
datdevboi <cami172013@gmail.com>
2018-12-17 17:38:37 -0600

    exported our new functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { _ } from "./_";
 import { add } from "./add";
 import { compose } from "./compose";
+import { join } from "./join";
+import { reverse } from "./reverse";
+import { split } from "./split";
+import { nativeInvoker } from "./nativeInvoker";
 
-export { _, add, compose };
+export { _, add, compose, join, reverse, split, nativeInvoker };
