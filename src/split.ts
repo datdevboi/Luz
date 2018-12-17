f2215fdc78f57943f6a0ba30d8e42e51695c58fa
datdevboi <cami172013@gmail.com>
2018-12-17 17:33:06 -0600

    Made use of our nativeInvoker function

--- src/split.ts
+++ src/split.ts
@@ -1,7 +1,10 @@
 import { _curry2 } from "./internals/_curry2";
+import { nativeInvoker } from "./nativeInvoker";
 
-const split = _curry2(function split(splitby: string | RegExp, a: string) {
-  return a.split(splitby);
-});
+// const split = _curry2(function split(splitby: string | RegExp, a: string) {
+//   return a.split(splitby);
+// });
+
+const split = nativeInvoker(1, "split");
 
 export { split };
